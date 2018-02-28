5d25f3f0d7506c1e725f44772a35314e06e8bf9a
Junior <Junior@LAPTOP-3HBQ8ARS>
2018-02-28 16:52:14 +0100

    Ya esta añadido las colisiones con la bala y los marcianos

--- src/codigo/VentanaJuego.java
+++ src/codigo/VentanaJuego.java
@@ -11,6 +11,7 @@ import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import javax.swing.Timer;
 
@@ -100,6 +101,25 @@ public class VentanaJuego extends javax.swing.JFrame {
         }
     }
 
+    private void chequeaColision() {
+        Rectangle2D.Double rectanguloMarciano = new Rectangle2D.Double();
+        Rectangle2D.Double rectanguloDisparo = new Rectangle2D.Double();
+        rectanguloDisparo.setFrame(miDisparo.getX(), miDisparo.getY(), miDisparo.imagen.getWidth(null), miDisparo.imagen.getHeight(null));
+        for (int i = 0; i < filasMarcianos; i++) {
+            for (int j = 0; j < columnasMarcianos; j++) {
+                rectanguloMarciano.setFrame(listaMarcianos[i][j].x, listaMarcianos[i][j].y, listaMarcianos[i][j].imagen.getWidth(null), listaMarcianos[i][j].imagen.getHeight(null));
+
+                if (rectanguloDisparo.intersects(rectanguloMarciano)) {
+                    //si esto es true es que los dos rectangulos han chocado
+                    listaMarcianos[i][j].y = 2000;
+                    //Si comentamos esto la bala atravesara los marcianos
+                    miDisparo.setY(2000);
+                    miDisparo.setDisparo(false);
+                }
+            }
+        }
+    }
+
     private void bucleDelJuego() {
         //Bucle que redibuja los objetos en el Jpanel1
         //primero borro todo lo que hay en el buffer
@@ -117,21 +137,8 @@ public class VentanaJuego extends javax.swing.JFrame {
 
         pintaMarcianos(g2);
 
-//        miMarciano.mueve();
-//
-//        if (contador < 50) {
-//            g2.drawImage(miMarciano.imagen, miMarciano.x, miMarciano.y, null);
-//        } else if (contador < 100) {
-//            g2.drawImage(miMarciano.imagen2, miMarciano.x, miMarciano.y, null);
-//        } else {
-//            contador = 0;
-//        }
-//
-//        if (miMarciano.x == ANCHOPANTALLA - miMarciano.imagen.getWidth(null) - 8 || miMarciano.x == 0) {
-//            miMarciano.direccion = !miMarciano.direccion;
-//            miMarciano.y += miMarciano.imagen.getHeight(null);
-//        }
-//-------------------------------------------------------------------------------------------------------------
+        chequeaColision();
+
 //Dibujo de golpe el buffer sobre el jPanel1
         g2 = (Graphics2D) jPanel1.getGraphics();
         g2.drawImage(buffer, 0, 0, null);
